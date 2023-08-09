3e9fc81dd6cc0e942c5f932442d23c2b968b9058
kennataddese6 <kennataddese6@gmail.com>
2023-08-09 10:45:03 +0300

    add server feature that detects client connection status

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const WebSocket = require("ws");
 
 const wss = new WebSocket.Server({ port: 8080 });
+console.log("server has started listening on port 8080");
 
 wss.on("connection", function connection(ws) {
   console.log("Client connected");
 
   ws.on("message", function incoming(message) {
+    ws.send("Got your message");
     console.log("received: %s", message);
   });
-
+  ws.on("close", function close() {
+    console.log("Client disconnected");
+  });
   ws.send("Hello, client!");
 });
