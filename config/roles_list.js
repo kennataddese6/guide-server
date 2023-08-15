c8e82021ee0037a3596ba7e246529bfd5516949f
kennataddese6 <kennataddese6@gmail.com>
2023-08-15 11:28:10 +0300

    add roles based on FLoors

--- config/roles_list.js
+++ config/roles_list.js
@@ -2,6 +2,7 @@ const ROLES_LIST = {
   FloorReceptionist: 4800,
   GroundReptionist: 1000,
   VipReceptionist: 1001,
+  Admin: 7706,
 };
 
 module.exports = ROLES_LIST;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,24 @@
+const ROLES_LIST = require("../config/roles_list");
 const User = require("../models/userModels");
 const asyncHandler = require("express-async-handler");
 
 const registerUser = asyncHandler(async (req, res) => {
   const { firstName, lastName, email, phoneNumber, floorNumber } = req.body;
+
+  const roles =
+    floorNumber >= 1 && floorNumber <= 48
+      ? ROLES_LIST.FloorReceptionist
+      : floorNumber == 0
+      ? ROLES_LIST.GroundReptionist
+      : floorNumber < 0
+      ? ROLES_LIST.VipReceptionist
+      : ROLES_LIST.Admin;
   console.log("here is the data", req.body);
   const user = await User.create({
     FirstName: firstName,
     LastName: lastName,
     Email: email,
-    Roles: 5050,
+    Roles: roles,
     PhoneNumber: phoneNumber,
     FloorNumber: floorNumber,
     Password: "Welcome2cbe",
@@ -20,7 +30,6 @@ const registerUser = asyncHandler(async (req, res) => {
     console.log("somethingwent wrong");
   }
 });
-
 
 module.exports = {
   registerUser,
