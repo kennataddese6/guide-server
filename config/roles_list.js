6cc10d29357101f12dd229a260a2d51ad3726c12
kennataddese6 <kennataddese6@gmail.com>
2023-08-15 10:57:49 +0300

    add role list for users and make phone number string

---
+++ config/roles_list.js
@@ -0,0 +1,7 @@
+const ROLES_LIST = {
+  FloorReceptionist: 4800,
+  GroundReptionist: 1000,
+  VipReceptionist: 1001,
+};
+
+module.exports = ROLES_LIST;
--- models/userModels.js
+++ models/userModels.js
@@ -18,7 +18,7 @@ const userSchema = mongoose.Schema(
       type: Number,
     },
     PhoneNumber: {
-      type: Number,
+      type: String,
     },
     FloorNumber: {
       type: Number,
