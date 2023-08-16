649c729843953ac82d89448d7f429e51749625d1
kennataddese6 <kennataddese6@gmail.com>
2023-08-16 14:22:14 +0300

    add customers in to the database

---
+++ controllers/customerController.js
@@ -0,0 +1,36 @@
+const Customer = require("../models/customerModel");
+const asyncHandler = require("express-async-handler");
+
+const registerCustomer = asyncHandler(async (req, res) => {
+  console.log("here is the request body", req.body);
+  const {
+    firstName,
+    lastName,
+    phoneNumber,
+    woreda,
+    subcity,
+    officeNumber,
+    department,
+    floorNumber,
+    elevatorNumber,
+  } = req.body;
+  const customer = await Customer.create({
+    FirstName: firstName,
+    LastName: lastName,
+    Woreda: woreda,
+    SubCity: subcity,
+    OfficeNumber: officeNumber,
+    PhoneNumber: phoneNumber,
+    FloorNumber: floorNumber,
+    Department: department,
+    ElevatorNumber: elevatorNumber,
+  });
+  if (customer) {
+    res.status(200).json(customer);
+  } else {
+    res.status(409);
+  }
+});
+module.exports = {
+  registerCustomer,
+};
