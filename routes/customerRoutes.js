6dee9e6351cb79309854c25de9f2a2a36be3027c
kennataddese6 <kennataddese6@gmail.com>
2023-08-18 15:45:38 +0300

    create a route to get floor customers

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -4,8 +4,10 @@ const router = express.Router();
 const {
   registerCustomer,
   getCustomers,
+  getFloorCustomers,
 } = require("../controllers/customerController");
 
 router.post("/", registerCustomer);
 router.get("/", getCustomers);
+router.get("/floorCustomers", getFloorCustomers);
 module.exports = router;
