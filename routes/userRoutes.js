d07ef3372a7cf2e6787b53407cbf2a874e78c364
kennataddese6 <kennataddese6@gmail.com>
2023-08-19 09:45:22 +0300

    create route to floor receptionists

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser } = require("../controllers/userController");
+const {
+  registerUser,
+  getFloorReceptionists,
+} = require("../controllers/userController");
 
 router.post("/", registerUser);
+router.get("/floorReceptionists", getFloorReceptionists);
 module.exports = router;
