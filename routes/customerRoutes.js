87f2a3b7171a6f977f5ac1adaa74cb03e3fc75d6
kennataddese6 <kennataddese6@gmail.com>
2023-08-21 10:16:18 +0300

    create a route to update customer

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -5,9 +5,12 @@ const {
   registerCustomer,
   getCustomers,
   getFloorCustomers,
+  updateCustomer,
 } = require("../controllers/customerController");
 
 router.post("/", registerCustomer);
 router.get("/", getCustomers);
+router.put("/", updateCustomer);
+
 router.get("/floorCustomers", getFloorCustomers);
 module.exports = router;
