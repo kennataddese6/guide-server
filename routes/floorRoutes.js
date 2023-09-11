9e1b83f86288a0147ae0f0010bc4c5bcb4d3854f
kennataddese6 <kennataddese6@gmail.com>
2023-09-11 12:59:27 +0300

    create a route to get floors

--- routes/floorRoutes.js
+++ routes/floorRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerFloor} = require("../controllers/floorController");
+const { registerFloor, getFloors } = require("../controllers/floorController");
 
 router.post("/", registerFloor);
+router.get("/", getFloors);
 
 module.exports = router;
