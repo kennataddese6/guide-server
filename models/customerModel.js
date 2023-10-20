f1d1a19ed84d00dc2b77c4c4c56e3b8f8868fa3e
KennaTaddese <KennaTaddese@cbe.com.et>
2023-10-20 17:03:07 +0300

    make subcity option string

--- models/customerModel.js
+++ models/customerModel.js
@@ -29,7 +29,7 @@ const CustomerSchema = mongoose.Schema(
       required: [true, "Please add Woreda"],
     },
     SubCity: {
-      type: Number,
+      type: String,
       required: [true, "Please add subCity"],
     },
     OfficeNumber: {
