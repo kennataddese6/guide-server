eeaf1351047a32837329110b4097c45523f15683
KennaTaddese <KennaTaddese@cbe.com.et>
2023-10-23 10:46:30 +0300

    create a route to update floor

--- routes/floorRoutes.js
+++ routes/floorRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerFloor, getFloors } = require("../controllers/floorController");
+const {
+  registerFloor,
+  getFloors,
+  updateFloor,
+} = require("../controllers/floorController");
 
 router.post("/", registerFloor);
 router.get("/", getFloors);
+router.put("/", updateFloor);
 
 module.exports = router;
